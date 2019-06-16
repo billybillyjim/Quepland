8c375e4b0909671e957511d3c6e6cd5ea78d8711
Luke <thereallmwels@gmail.com>
2019-06-15 20:04:38 -0400

    Updated message manager, balanced items.

--- Services/MessageManager.cs
+++ Services/MessageManager.cs
@@ -8,24 +8,31 @@ public class MessageManager
     public event EventHandler StateChanged;
     private int maxMessages = 12;
     private List<Message> messages = new List<Message>();
+    private string lastMessage;
+    private int repeatMessageCount = 1;
     public void AddMessage(string newMessageString)
     {
-        Message newMessage = new Message(newMessageString);
-        messages.Add(newMessage);
-        if (messages.Count >= maxMessages)
-        {
-            messages.Remove(messages[0]);
-        }
-        StateHasChanged();
+        AddMessage(newMessageString, "black");
     }
     public void AddMessage(string newMessageString, string color)
     {
         Message newMessage = new Message(newMessageString, color);
-        messages.Add(newMessage);
-        if (messages.Count >= maxMessages)
+        if (lastMessage == newMessageString)
         {
-            messages.Remove(messages[0]);
+            repeatMessageCount++;
+            messages.Last().messageText =  lastMessage + "(" + repeatMessageCount + ")";         
         }
+        else
+        {
+            repeatMessageCount = 1;
+            messages.Add(newMessage);
+            if (messages.Count >= maxMessages)
+            {
+                messages.Remove(messages[0]);
+            }
+        }
+
+        lastMessage = newMessage.messageText;
         StateHasChanged();
     }
     public List<Message> GetMessages()
