5653221c0725599a0b40cafc4a2959503a6a2638
Luke <thereallmwels@gmail.com>
2019-06-24 21:27:33 -0400

    added update number

--- Services/GameState.cs
+++ Services/GameState.cs
@@ -30,6 +30,7 @@ public class GameState
     public bool saveGameExists;
 
     public string previousURL;
+    public string updateVersionString = "Update (1.013a)";
     
     public GameItem currentUsedItem;
     public GameItem currentGatherItem;
