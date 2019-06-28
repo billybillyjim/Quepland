93cb38739a7dd7d48db84a3337c228fe011a4372
Luke <thereallmwels@gmail.com>
2019-06-27 20:11:57 -0400

    bug fixees, text updated

--- wwwroot/kongregateFunctions.js
+++ wwwroot/kongregateFunctions.js
@@ -4,7 +4,14 @@
         // You can now access the Kongregate API with:
         // kongregate.services.getUsername(), etc
         // Proceed with loading your game...
-        console.log(window.kongregate.services.getUsername());
+        if (kongregate.services.isGuest()) {
+            return "You are a guest.";
+        } else {
+            return kongregate.getUsername();
+        }
+    },
+    updateTotalLevelScore: function (totalLevel) {
+        kongregate.stats.submit("Total Level", totalLevel);
     }
 
 };
