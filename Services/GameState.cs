8d1f1d83d0ee89be629fab37bcfc8fd266ea9acf
Luke <thereallmwels@gmail.com>
2019-06-28 21:28:31 -0400

    Fixed a bug that checked for the wrong level

--- Services/GameState.cs
+++ Services/GameState.cs
@@ -34,7 +34,7 @@ public class GameState
     public bool saveGameExists;
 
     public string previousURL;
-    public string updateVersionString = "1.017a";
+    public string updateVersionString = "1.017b";
     
     public GameItem currentUsedItem;
     public GameItem currentGatherItem;
