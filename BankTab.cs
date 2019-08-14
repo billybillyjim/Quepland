cf766ef3223f6792f856cf1dae4240f13d63e321
Saulius Šaltenis <31516441+LtLi0n@users.noreply.github.com>
2019-08-14 16:00:59 +0300

    Forgot to initialize Name.

--- BankTab.cs
+++ BankTab.cs
@@ -2,11 +2,12 @@
 
 public class BankTab
 {
-	public string Name { get; set; }
+    public string Name { get; set; }
     public List<int> ItemIDs { get; }
 
     public BankTab(string name)
     {
+	Name = name;
         ItemIDs = new List<int>();
     }
 }
